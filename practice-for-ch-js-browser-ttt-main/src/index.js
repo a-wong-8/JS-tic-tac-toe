9f5c7d8693c59db05cc72b7050876811b58f14ca
a-wong-8 <awong88@berkeley.edu>
2023-05-23 14:55:42 -0700

    done

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,10 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import Game from "./ttt_node/game.js"
+import View from "./ttt-view.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const container = document.querySelector("figure.ttt");
+  const game = new Game(); 
+  const view = new View(game, container); 
 });
+
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,57 @@
 class View {
   constructor(game, el) {
+    this.setupBoard(el);
+    this.game = game;
+    this.value = "X";
   }
   
-  setupBoard() {
+  setupBoard(el) {
+    const ul = document.createElement('ul');
+    ul.classList.add("square-list");
+    // const figure = document.querySelector('figure.ttt');
+    el.appendChild(ul);
+    // figure.appendChild(ul);
+
+    for (let i = 0; i < 9; i++) {
+      const li = document.createElement('li');
+      li.setAttribute("data-pos", i);
+      ul.appendChild(li);
+      li.innerText = "N"
+    }
+
+    ul.addEventListener("click", event => {
+      this.handleClick(event.target);
+      if (this.game.isOver()) {
+        this.handleGameOver()
+      }
+    })
   }
   
   handleClick(e) {
+    const x = Math.floor(e.getAttribute("data-pos")/3);
+    const y = e.getAttribute("data-pos") % 3;
+    this.game.playMove([x, y]);
+    e.innerText = this.value;
+    this.value = (this.value === "X") ? "O" : "X";
+    e.classList.add("selected");
   }
 
-  makeMove(square) {
+  makeMove(square) {  
   }
   
   handleGameOver() {
+    this.value = (this.value === "X") ? "O" : "X";
+    const winningSquares = document.querySelectorAll(".selected");
+    for (let i = 0; i < winningSquares.length; i++) { 
+      const element = winningSquares[i];
+      if (element.innerText === this.value) {
+        element.classList.remove("selected");
+        element.classList.add("completed");
+      } else {
+        element.classList.remove("selected");
+        element.classList.add("wrong");
+      }
+    }
   }
 }
 
